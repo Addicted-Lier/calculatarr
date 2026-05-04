a21bd94aad707038cfea099d74d0608bd2dd7ee8
Md Shahjahan <s.aliforgaming1235@gmail.com>
2026-05-04 16:46:46 +0530

    so basically added a quit button in flow and made it continous loop for better execution everytime

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
-op = input("Enter what you wish to do +,-,*,/ : ")
-a = int(input("enter the first number for operation : "))
-b = int(input("enter the second number for operation : "))
 
-if op == "+":
-    ans = a+b
-    print (ans)
-elif op =="-":
-    ans = a-b
-    print(ans)
-elif op =="*":
-    ans = a*b
-    print(ans)
-elif op =="/":
-    ans = a/b
-    print(ans)
-elif op =="%":
-    ans = a%b
-    print(ans)
+while True:
+    kuit = input("enter 'q' to quit the calculator or press any other key to continue : ")
+    
+    
 
+    if kuit.lower() == 'q':
+        print("quiting the calculator")
+        break
+    
+
+    op = input("Enter what you wish to do +,-,*,/ :  ")
+    a = int(input("enter the first number for operation : "))
+    b = int(input("enter the second number for operation : "))
+
+    if op == "+":
+        ans = a+b
+        print (ans)
+    elif op =="-":
+        ans = a-b
+        print(ans)
+    elif op =="*":
+        ans = a*b
+        print(ans)
+    elif op =="/":
+        ans = a/b
+        print(ans)
+    elif op =="%":
+        ans = a%b
+        print(ans)
+    
+   

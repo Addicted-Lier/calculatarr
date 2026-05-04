b7404dc4426858dcbbf804ce8d998133844d0699
Md Shahjahan <s.aliforgaming1235@gmail.com>
2026-05-04 16:18:16 +0530

    jst made the v1 of the calculatarr

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from calculatarrr!")
+op = input("Enter what you wish to do +,-,*,/ : ")
+a = int(input("enter the first number for operation : "))
+b = int(input("enter the second number for operation : "))
 
+if op == "+":
+    ans = a+b
+    print (ans)
+elif op =="-":
+    ans = a-b
+    print(ans)
+elif op =="*":
+    ans = a*b
+    print(ans)
+elif op =="/":
+    ans = a/b
+    print(ans)
+elif op =="%":
+    ans = a%b
+    print(ans)
 
-if __name__ == "__main__":
-    main()

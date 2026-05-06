8634db08cc1ebaa59f419c0cccbb2727deff3e9f
Md Shahjahan <s.aliforgaming1235@gmail.com>
2026-05-06 16:44:28 +0530

    so tried to make it follow bodmas rule half job for today is done as it detects '*' and '\' with operator precedence next job is to add or sub the operated value also added notes file to log the bugs and learning

--- main.py
+++ main.py
@@ -21,24 +21,46 @@ while True:
             converted.append(number)
         else:
             converted.append(item)
+    i = 0
+    while i < (len(converted)):
+        if converted[i] == "*" :
+            num1 = converted[i-1]
+            num2 = converted[i+1]
+            result = num1*num2
+            converted[i-1 : i+2] = [result]
+            i=0
+            continue
 
-
+        elif converted[i] == "/" :
+            num1 = converted[i-1]
+            num2 = converted[i+1]
+            if num2 !=0:
+                result = num1/num2
+            else:
+                # exptn = "Maybe you have put 0, dividing with 0 = nada."
+                print("dividing a number with 0 = nada \n exiting the calculator")
+                break
+            converted[i-1 : i+2] = [result]
+            i=0
+            continue
+        i+=1
     # for items in converted:
     #     print(items)
-    if converted[1] == '+':
-        ans = converted[0]+converted[2]
-        print (ans)
-    elif converted[1] =="-":
-        ans = converted[0]-converted[2]
-        print(ans)
-    elif converted[1] =="*":
-        ans = converted[0]*converted[2]
-        print(ans)
-    elif converted[1] =="/":
-        ans = converted[0]/converted[2]
-        print(ans)
-    elif converted[1] =="%":
-        ans = converted[0]%converted[2]
-        print(ans)
+    print(converted)
+    # if converted[1] == '+':
+    #     ans = converted[0]+converted[2]
+    #     print (ans)
+    # elif converted[1] =="-":
+    #     ans = converted[0]-converted[2]
+    #     print(ans)
+    # elif converted[1] =="*":
+    #     ans = converted[0]*converted[2]
+    #     print(ans)
+    # elif converted[1] =="/" and converted[2]!=0:
+    #     ans = converted[0]/converted[2]
+    #     print(ans)
+    # elif converted[1] =="%":
+    #     ans = converted[0]%converted[2]
+    #     print(ans)
     
-   
+    
